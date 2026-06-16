007a2d0941c925b9f239f09a5093341f0c0e3619
Sören Anspach <soeren.anspach@tuhh.de>
2026-06-16 15:00:26 +0200

    Add handeye package

---
+++ rnm-workspace-main/ros2_ws/src/rnm_handeye/rnm_handeye/calc_handeye.py
@@ -0,0 +1,60 @@
+import cv2
+import numpy as np
+import yaml
+from ament_index_python.packages import get_package_share_directory
+import os
+
+package_path = get_package_share_directory("rnm_handeye")
+
+sample_file = os.path.join(package_path, "config", "handeye_samples.npz")
+result_file = os.path.join(package_path, "config", "handeye_result.yml")
+
+
+# Load samples
+data = np.load(sample_file)
+
+gripper2base = data["gripper2base_poses"]
+target2cam = data["target2cam_poses"]
+
+print(f"Loaded {len(gripper2base)} samples")
+
+
+# Split transformations
+R_gripper2base = []
+t_gripper2base = []
+R_target2cam = []
+t_target2cam = []
+
+for T in gripper2base:
+    R_gripper2base.append(T[:3,:3])
+    t_gripper2base.append(T[:3,3])
+
+for T in target2cam:
+    R_target2cam.append(T[:3,:3])
+    t_target2cam.append(T[:3,3])
+
+
+# Hand-eye calibration
+R_cam2gripper, t_cam2gripper = cv2.calibrateHandEye(R_gripper2base, t_gripper2base, R_target2cam, t_target2cam, method=cv2.CALIB_HAND_EYE_TSAI)
+
+
+# Save result
+result = {
+    "handeye_calibration":
+    {
+        "translation":
+        {
+            "x": float(t_cam2gripper[0]),
+            "y": float(t_cam2gripper[1]),
+            "z": float(t_cam2gripper[2]),
+        },
+        "rotation_matrix":
+        R_cam2gripper.tolist(),
+    }
+}
+
+with open(result_file, "w") as f:
+    yaml.dump(result, f)
+
+print("Calibration calculated")
+print("Saved:", result_file)
