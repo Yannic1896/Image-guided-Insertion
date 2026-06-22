07ca4965b926b3bb7eccaa9ed5092015d194e33a
Sören Anspach <soeren.anspach@tuhh.de>
2026-06-22 22:09:01 +0200

    Collector triggers on scanning node message and saves raw images

--- rnm-workspace-main/ros2_ws/src/rnm_handeye/rnm_handeye/calc_handeye.py
+++ rnm-workspace-main/ros2_ws/src/rnm_handeye/rnm_handeye/calc_handeye.py
@@ -4,57 +4,61 @@ import yaml
 from ament_index_python.packages import get_package_share_directory
 import os
 
-package_path = get_package_share_directory("rnm_handeye")
+def main(args = None):
 
-sample_file = os.path.join(package_path, "config", "handeye_samples.npz")
-result_file = os.path.join(package_path, "config", "handeye_result.yml")
+    package_path = get_package_share_directory("rnm_handeye")
 
-
-# Load samples
-data = np.load(sample_file)
-
-gripper2base = data["gripper2base_poses"]
-target2cam = data["target2cam_poses"]
-
-print(f"Loaded {len(gripper2base)} samples")
+    sample_file = os.path.join(package_path, "config", "handeye_samples.npz")
+    result_file = os.path.join(package_path, "config", "handeye_result.yml")
 
 
-# Split transformations
-R_gripper2base = []
-t_gripper2base = []
-R_target2cam = []
-t_target2cam = []
+    # Load samples
+    if not os.path.exists(sample_file):
+        print("No handeye samples found.")
+        return
+    
+    data = np.load(sample_file, allow_pickle=True)
 
-for T in gripper2base:
-    R_gripper2base.append(T[:3,:3])
-    t_gripper2base.append(T[:3,3])
+    gripper2base = data["gripper2base_poses"]
+    target2cam = data["target2cam_poses"]
 
-for T in target2cam:
-    R_target2cam.append(T[:3,:3])
-    t_target2cam.append(T[:3,3])
+    print(f"Loaded {len(gripper2base)} samples")
 
 
-# Hand-eye calibration
-R_cam2gripper, t_cam2gripper = cv2.calibrateHandEye(R_gripper2base, t_gripper2base, R_target2cam, t_target2cam, method=cv2.CALIB_HAND_EYE_TSAI)
+    # Split transformations
+    R_gripper2base = []
+    t_gripper2base = []
+    R_target2cam = []
+    t_target2cam = []
+
+    for T in gripper2base:
+        R_gripper2base.append(T[:3,:3])
+        t_gripper2base.append(T[:3,3])
+
+    for T in target2cam:
+        R_target2cam.append(T[:3,:3])
+        t_target2cam.append(T[:3,3])
 
 
-# Save result
-result = {
-    "handeye_calibration":
-    {
-        "translation":
+    # Hand-eye calibration
+    R_cam2gripper, t_cam2gripper = cv2.calibrateHandEye(R_gripper2base, t_gripper2base, R_target2cam, t_target2cam, method=cv2.CALIB_HAND_EYE_TSAI)
+    T_cam2gripper = np.eye(4)
+    T_cam2gripper[:3,:3] = R_cam2gripper
+    T_cam2gripper[:3,3] = t_cam2gripper.flatten()
+
+    # Save result
+    result = {
+        "handeye_calibration":
         {
-            "x": float(t_cam2gripper[0]),
-            "y": float(t_cam2gripper[1]),
-            "z": float(t_cam2gripper[2]),
-        },
-        "rotation_matrix":
-        R_cam2gripper.tolist(),
+            "transformation_matrix": T_cam2gripper.tolist()
+        }
     }
-}
 
-with open(result_file, "w") as f:
-    yaml.dump(result, f)
+    with open(result_file, "w") as f:
+        yaml.dump(result, f)
 
-print("Calibration calculated")
-print("Saved:", result_file)
+    print("Calibration calculated")
+    print("Saved:", result_file)
+
+if __name__ == "__main__":
+    main()
